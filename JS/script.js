a8ac5c603ba55500c35cd5654efcb9d733152fb0
manahil <your-email@example.com>
2024-10-26 13:29:43 +0400

    Added files with LFS tracking

--- JS/script.js
+++ JS/script.js
@@ -1,15 +1,3 @@
-const bar = document.getElementById('bar');
-const close = document.getElementById('close');
-const nav = document.getElementById('navbar');
-
-if (bar) {
-    bar.addEventListener('click', () => {
-        nav.classList.add('active');
-    })
-}
-
-if (close) {
-    close.addEventListener('click', () => {
-        nav.classList.remove('active');
-    })
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:c95c44b1aa88e3001103c4d9c7236090c0e455dabd5988f760889a49fb2fff0c
+size 345
